e92335109941c02af5124e10ffaba2202fc9ee7c
Batkhuu Byambajav <batkhuu10@gmail.com>
2026-08-23 09:50:23 +0900

    feat: Add build and push script for Docker images

---
+++ scripts/build-push.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+## --- Base --- ##
+_SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]:-"$0"}")" >/dev/null 2>&1 && pwd -P)"
+_PROJECT_DIR="$(cd "${_SCRIPT_DIR}/.." >/dev/null 2>&1 && pwd)"
+cd "${_PROJECT_DIR}" || exit 2
+
+
+# shellcheck disable=SC1091
+[ -f .env ] && . .env
+
+
+# Checking docker and docker-compose installed:
+if ! command -v docker >/dev/null 2>&1; then
+	echo "[ERROR]: Not found 'docker' command, please install it first!" >&2
+	exit 1
+fi
+
+if ! docker info > /dev/null 2>&1; then
+	echo "[ERROR]: Unable to communicate with the docker daemon. Check docker is running or check your account added to docker group!" >&2
+	exit 1
+fi
+
+if ! docker compose > /dev/null 2>&1; then
+	echo "[ERROR]: 'docker compose' not found or not installed!" >&2
+	exit 1
+fi
+## --- Base --- ##
+
+
+## --- Variables --- ##
+IMG_VERSION=1.9.2
+## --- Variables --- ##
+
+docker build -t outline-base:local -f Dockerfile.base .
+
+docker build -t bybatkhuu/outline:${IMG_VERSION} --build-arg BASE_IMAGE=outline-base:local .
+
+docker push bybatkhuu/outline:${IMG_VERSION}
